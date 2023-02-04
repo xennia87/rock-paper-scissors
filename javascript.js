5453c05c661372e0a82ad9e54081b5cf000e992a
xennia87 <62959606+xennia87@users.noreply.github.com>
2023-02-04 23:00:05 +0100

    Sin funcion final

---
+++ javascript.js
@@ -0,0 +1,63 @@
+function getComputerChoice() {
+    let options = ["rock", "paper", "scissors"];
+    let randomIndex = Math.floor(Math.random()*options.length);
+    let selection = options[randomIndex];
+    return selection;
+}
+
+let computerSelection = getComputerChoice();
+
+function playRound(computerSelection, playerSelection) {
+  let result;
+    if (computerSelection == "rock") {
+        if (playerSelection == "rock") {
+            result = 0
+        } else if (playerSelection == "paper") {
+            result = 1
+        } else if (playerSelection == "scissors") {
+            result = -1
+        }
+    } else if (computerSelection == "paper") {
+        if (playerSelection == "rock") {
+            result = -1;
+        } else if (playerSelection == "paper") {
+            result = 0;
+        } else if (playerSelection == "scissors") {
+            result = 1;
+        }
+    } else if (computerSelection == "scissors") {
+        if (playerSelection == "rock") {
+            result = 1;
+        } else if (playerSelection == "paper") {
+            result = -1;
+        } else if (playerSelection == "scissors") {
+            result = 0;
+        }
+    }
+    return result;
+}
+
+
+let playerPoints = 0;
+let computerPoints = 0;
+for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt("Choose Rock, Paper or Scissors").toLowerCase();
+    computerSelection = getComputerChoice();
+    console.log(`Computer choses ${computerSelection}`);
+    let result = playRound(computerSelection, playerSelection);
+    if (result > 0) {
+        ++playerPoints;
+        console.log(`Player wins. Computer has ${computerPoints} points. Player has ${playerPoints}`);
+    } else if (result < 0) {
+        ++computerPoints;
+        console.log(`Computer wins. Computer has ${computerPoints} points. Player has ${playerPoints}`);
+    } else {
+        console.log("It's a tie. Play again")
+    }
+}
+
+if (computerPoints > playerPoints) {
+    console.log("Computer wins");
+} else {
+    console.log("Player wins");
+}
